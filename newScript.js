35930304125402e2670a73862d59a1498c5b5b84
enickel <emmajanenickel@gmail.com>
2022-07-20 11:42:46 +1000

    New JS file created

---
+++ newScript.js
@@ -0,0 +1,18 @@
+console.log("Hello World);
+function dispMessage(time){
+let morningMsg = "Good Morning";
+let eveningMsg = "Good Evening";
+	switch(time){
+		case "am":
+			console.log(morningMsg);
+			break;
+		case "pm":
+			console.log(eveningMsg);
+			break
+		default:
+			console.log("Invalid entry);
+}
+}
+
+
+
